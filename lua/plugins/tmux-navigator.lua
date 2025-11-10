b2ec2301f29446e50ebe3a314ccee8b9930eaa97
edilsonlonc <eddylson.londonio@gmail.com>
2025-11-10 10:03:17 -0500

    add plugins

---
+++ lua/plugins/tmux-navigator.lua
@@ -0,0 +1,10 @@
+return {
+  "christoomey/vim-tmux-navigator",
+  keys = {
+    { "<C-h>", "<cmd>TmuxNavigateLeft<CR>", desc = "Tmux left" },
+    { "<C-j>", "<cmd>TmuxNavigateDown<CR>", desc = "Tmux down" },
+    { "<C-k>", "<cmd>TmuxNavigateUp<CR>", desc = "Tmux up" },
+    { "<C-l>", "<cmd>TmuxNavigateRight<CR>", desc = "Tmux right" },
+    { "<C-\\>", "<cmd>TmuxNavigatePrevious<CR>", desc = "Tmux previous" },
+  },
+}
